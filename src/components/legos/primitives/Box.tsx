00ba42b6224bf203a49e66d79395d16106ad0bdd
Chuck Dries <chuck@chuckdries.com>
2019-01-29 13:35:02 -0700

    flex

--- src/components/legos/primitives/Box.tsx
+++ src/components/legos/primitives/Box.tsx
@@ -1,37 +1,6 @@
 import styled from 'styled-components';
-import {
-  AlignContentProps,
-  AlignItemsProps,
-  AlignSelfProps,
-  flex,
-  FlexBasisProps,
-  FlexDirectionProps,
-  FlexWrapProps,
-  JustifyContentProps,
-  JustifySelfProps,
-} from 'styled-system';
 import { LAYOUT, LayoutProps } from './constants';
 
 export const Box = styled.div<LayoutProps>`
   ${LAYOUT};
 `;
-
-type FlexContainerProps =
-  FlexDirectionProps &
-  FlexWrapProps &
-  FlexBasisProps &
-  AlignItemsProps &
-  AlignContentProps &
-  JustifySelfProps &
-  AlignSelfProps &
-  JustifyContentProps;
-
-export const Flex = styled('p')<LayoutProps & FlexContainerProps>`
-  display: flex;
-  ${flex}
-`;
-
-Flex.defaultProps = {
-  display: 'block',
-  flexDirection: 'row',
-};
